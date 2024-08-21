a680d9b7c7044b0febcc005d4d782ecca0a272b2
HyunKook <gusrnr5153@gmail.com>
2024-08-21 18:23:05 +0900

    GetMapping - 다건조회

--- src/main/java/com/sparta/basicspringsession/controller/MemberController.java
+++ src/main/java/com/sparta/basicspringsession/controller/MemberController.java
@@ -2,11 +2,14 @@ package com.sparta.basicspringsession.controller;
 
 import com.sparta.basicspringsession.dto.MemberSaveRequestDto;
 import com.sparta.basicspringsession.dto.MemberSaveResponseDto;
+import com.sparta.basicspringsession.dto.MemberSimpleResponseDto;
 import com.sparta.basicspringsession.repository.MemberRepository;
 import com.sparta.basicspringsession.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +19,11 @@ public class MemberController {
     @PostMapping("/members")
     public ResponseEntity<MemberSaveResponseDto> saveMember (@RequestBody MemberSaveRequestDto requestDto){
         return ResponseEntity.ok(memberService.saveMember(requestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberSimpleResponseDto>> getMembers(){
+        return ResponseEntity.ok(memberService.getMembers());
     }
 
 }
--- src/main/java/com/sparta/basicspringsession/service/MemberService.java
+++ src/main/java/com/sparta/basicspringsession/service/MemberService.java
@@ -2,11 +2,15 @@ package com.sparta.basicspringsession.service;
 
 import com.sparta.basicspringsession.dto.MemberSaveRequestDto;
 import com.sparta.basicspringsession.dto.MemberSaveResponseDto;
+import com.sparta.basicspringsession.dto.MemberSimpleResponseDto;
 import com.sparta.basicspringsession.entity.Member;
 import com.sparta.basicspringsession.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -19,6 +23,17 @@ public class MemberService {
         Member member = new Member(requestDto.getName());
         Member savedMember = memberRepository.save(member);
         return new MemberSaveResponseDto(member.getId(), member.getName());
+    }
+
+    public List<MemberSimpleResponseDto> getMembers(){
+        List<Member> members = memberRepository.findAll();
+        List<MemberSimpleResponseDto> memberList = new ArrayList<>();
+        for (Member member : members) {
+            MemberSimpleResponseDto responseDto = new MemberSimpleResponseDto();
+            memberList.add(responseDto);
+        }
+        return memberList;
+
     }
 
 
